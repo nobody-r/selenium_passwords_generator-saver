ea37f6289c6ad9b26d28d801b414c88136f39b47
nobody-r <coparei7@gmail.com>
2020-06-28 16:26:47 +0200

    pass

--- pssw_gen/sav.py
+++ pssw_gen/sav.py
@@ -11,17 +11,18 @@ options.add_argument("--headless")  #OPTION THAT RUN FIREFOX IN BACKGROUND (INST
 browserop = webdriver.Chrome(executable_path=r'INSERT YOUR GECKODRIVER PATH', options=options)
 browserop.get('https://toolset.mrwebmaster.it/varie/password-generator.html')   #WEB SERVICE LINK
 canclungh=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[1]/input').clear()
-inslungh=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[1]/input').send_keys("16") #NUMBER OF CHARACTERS
-nomin=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[3]/div/div[2]/label').click() #DEACTIVATE LOWERCASE LETTERS
-nocar=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[6]/div/div[2]/label').click()  #DEACTIVATE SYMBOLS
-workbook= xlsxwriter.Workbook('YOUR EXCEL PATH') #EXCEL FILE TO SAVE THE PASSWORDS
-worksheet_pass= workbook.add_worksheet('NAME OF SHEET') #CREATES A NEW EXCEL SHEET
+inslungh=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[1]/input').send_keys("16")    #NUMBER OF CHARACTERS
+nomin=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[3]/div/div[2]/label').click()    #DEACTIVATE LOWERCASE LETTERS
+nocar=browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[6]/div/div[2]/label').click()    #DEACTIVATE SYMBOLS
+workbook= xlsxwriter.Workbook('YOUR EXCEL PATH')    #EXCEL FILE TO SAVE THE PASSWORDS
+worksheet_pass= workbook.add_worksheet('NAME OF SHEET')     #CREATES A NEW EXCEL SHEET
 
-numpass= 25000 #number of passwords that will be generated and saved
+
+numpass= 25000  #number of passwords that will be generated and saved
 exc_row = 0
 exc_column = 0
 
-while numpass!=0:     #LOOP
+while numpass!=0:       #LOOP
     #time.sleep(0.2)    UNCOMMENT IF YOU WANT TO MAKE IT SLOWER
     gen = browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[7]/div[1]/button').click()   #GENERATE BUTTON
     copy = browserop.find_element_by_xpath('/html/body/div[2]/section/div[1]/div[3]/div[7]/div[2]/div/pre')         #SAVES THE PASSWORD INTO A VARIABLE
